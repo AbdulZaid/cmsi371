d878b47fa050fcb3611b302d3d5c25e3b0196094
Abdul Alzaid <aalzaid1@lion.lmu.edu>
2014-01-30 01:20:52 -0800

    Working on the lines and adding constructing a curve object.

--- homework/sprites/quadCurves.js
+++ homework/sprites/quadCurves.js
@@ -6,18 +6,23 @@
 (function () {
     // Ditto on using jQuery here.
     var canvas = document.getElementById("canvas"),
-        renderingContext = canvas.getContext("2d"),
-
+        renderingContext = canvas.getContext("2d");
+        curves = {
+            startPoint: {x: 100, y:100},
+            controlPoint1: {x: 300, y:100},
+            widthOfLine: 4,
+            strokeColor: "black"
+        }
         renderingContext.beginPath();
         renderingContext.strokeStyle = "blue";
-        renderingContext.moveTo(100, 100);
-        renderingContext.lineTo(300, 100);
+        renderingContext.moveTo(curves.startPoint.x, curves.startPoint.y);
+        renderingContext.lineTo(curves.controlPoint1.x, curves.controlPoint1.y);
         renderingContext.stroke();
         // Draw a quadratic curve by using the same line coordinates.
         renderingContext.beginPath();
-        renderingContext.lineWidth = "3";
-        renderingContext.strokeStyle = "black";
-        renderingContext.moveTo(100, 100);
+        renderingContext.lineWidth = curves.widthOfLine;
+        renderingContext.strokeStyle = curves.strokeColor;
+        renderingContext.moveTo(curves.startPoint.x, curves.startPoint.y);
         renderingContext.quadraticCurveTo(200, 300, 300, 100);
         renderingContext.stroke();
 }());
