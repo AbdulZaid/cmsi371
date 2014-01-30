9b7ba355125c3aad279913eb3df7097b884145af
Abdul Alzaid <aalzaid1@lion.lmu.edu>
2014-01-30 01:26:10 -0800

    Working on the quadratic curve dimensions. Adding values to object

--- homework/sprites/quadCurves.js
+++ homework/sprites/quadCurves.js
@@ -10,19 +10,21 @@
         curves = {
             startPoint: {x: 100, y:100},
             controlPoint1: {x: 300, y:100},
+            controlPoint2: {cp1x:200, cp1y:300, x:300, y:100},
             widthOfLine: 4,
-            strokeColor: "black"
+            colors: {black:"black", blue:"blue", red:"red"}
         }
         renderingContext.beginPath();
-        renderingContext.strokeStyle = "blue";
+        renderingContext.strokeStyle = curves.colors.blue;
         renderingContext.moveTo(curves.startPoint.x, curves.startPoint.y);
         renderingContext.lineTo(curves.controlPoint1.x, curves.controlPoint1.y);
         renderingContext.stroke();
         // Draw a quadratic curve by using the same line coordinates.
         renderingContext.beginPath();
         renderingContext.lineWidth = curves.widthOfLine;
-        renderingContext.strokeStyle = curves.strokeColor;
+        renderingContext.strokeStyle = curves.colors.black;
         renderingContext.moveTo(curves.startPoint.x, curves.startPoint.y);
-        renderingContext.quadraticCurveTo(200, 300, 300, 100);
+        renderingContext.quadraticCurveTo(curves.controlPoint2.cp1x, curves.controlPoint2.cp1y,
+                                          curves.controlPoint2.x, curves.controlPoint2.y);
         renderingContext.stroke();
 }());
